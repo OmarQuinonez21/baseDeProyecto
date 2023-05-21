96b435078e1c56360639a9cb5cd38e91095846ef
Jetix24 <josepabloaguirre02@gmail.com>
2023-05-21 15:32:56 -0600

    Add: RegistroActivity

--- app/src/main/java/com/example/inicio/IniciarSesionActivity.java
+++ app/src/main/java/com/example/inicio/IniciarSesionActivity.java
@@ -2,13 +2,30 @@ package com.example.inicio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IniciarSesionActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+        btnLogin=findViewById(R.id.log);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                 startActivity(intent);
+            }
+        });
     }
+
+
+
 }
