99d63c8fc26ae3b9be6afae2fa94139f695d0eb5
Omar <Oqekrt407@gmail.com>
2023-05-21 01:10:17 -0700

    Cambio a de fragment a activities

---
+++ app/src/main/java/com/example/inicio/usuarioActivity.java
@@ -0,0 +1,15 @@
+package com.example.inicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class usuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+
+    }
+}
